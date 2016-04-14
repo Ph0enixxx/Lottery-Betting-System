248d8be7c9500fb3e1368d3b05ef7345e6f7c1ad
梁家瑞 <ljracm@gmail.com>
2016-04-14 22:49:28 +0800

    ADD: less support

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,16 +3,6 @@
 module.exports = function(grunt) {
   grunt.initConfig({
     pkg: grunt.file.readJSON('package.json'),
-    jshint: {
-      build: [
-        '*.js',
-        'public/**/*.js',
-        '!public/bower_components/**/*.js'
-      ],
-      options: {
-        jshintrc: '.jshintrc'
-      }
-    },
     less: {
       development: {
         files: {
@@ -32,7 +22,7 @@ module.exports = function(grunt) {
           '!public/css/main.css',
           'index.js'
         ],
-        tasks: ['jshint', 'less:development', 'express:development:stop', 'express:development']
+        tasks: ['less:development', 'express:development:stop', 'express:development']
       },
       express: {
         files: [
@@ -53,12 +43,11 @@ module.exports = function(grunt) {
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-less');
   grunt.loadNpmTasks('grunt-contrib-watch');
 
   grunt.loadNpmTasks('grunt-express-server');
 
-  grunt.registerTask('default', ['jshint', 'less:development', 'express:development', 'watch']);
-  grunt.registerTask('production', ['jshint', 'less:production']);
+  grunt.registerTask('default', ['less:development', 'express:development', 'watch']);
+  grunt.registerTask('production', ['less:production']);
 };
