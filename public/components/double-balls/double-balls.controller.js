7a1e6f5d6e057b0cd22b574887144478541828b8
梁家瑞 <ljracm@gmail.com>
2016-04-17 23:27:21 +0800

    ADD: 点击双色球交互

--- public/components/double-balls/double-balls.controller.js
+++ public/components/double-balls/double-balls.controller.js
@@ -1,17 +1,57 @@
 (function() {
   angular.module('LotteryApp').controller('DoubleBallsController', DoubleBallsController);
 
-  function DoubleBallsController() {
-    this.tabs = [
+  function DoubleBallsController(appUtils) {
+    var RED_BALL_NUM = 33;
+    var BLUE_BALL_NUM = 16;
+    var NEED_RED_NUM = 6;
+    var ballCtrl = this;
+    var RED_BALL = 0;
+    var BLUE_BALL = 1;
+    var PER_PRICE = 2;
+
+    ballCtrl.selectBall = selectBall;
+    ballCtrl.colorBallStat = [0, 0];
+    ballCtrl.money = 0;
+    ballCtrl.betCount = 0;
+    ballCtrl.tabs = [
       { title: 'Dynamic Title 1', content: 'Dynamic content 1' },
       { title: 'Dynamic Title 2', content: 'Dynamic content 2', disabled: true }
     ];
 
-    this.model = {
+    ballCtrl.model = {
       name: 'Tabs'
     };
 
-    this.redBalls = _.range(33)
-    this.blueBalls = _.range(16);
+    ballCtrl.redBalls = initBalls(RED_BALL_NUM);
+    ballCtrl.blueBalls = initBalls(BLUE_BALL_NUM); 
+    ballCtrl.redAwardBalls = _.range(1,7);
+    ballCtrl.blueBall = 2;
+
+    function selectBall(ball, color) {
+      if (ball.selected) {
+        ball.selected  = false;
+        ballCtrl.colorBallStat[color]--;
+      }
+      else {
+        ball.selected  = true;
+        ballCtrl.colorBallStat[color]++;
+      }
+      ballCtrl.betCount  = appUtils.computeCxy(ballCtrl.colorBallStat[RED_BALL], NEED_RED_NUM) * ballCtrl.colorBallStat[BLUE_BALL];
+      debugger;
+      ballCtrl.money =  ballCtrl.betCount * PER_PRICE;
+    }
+
+    function initBalls(num){
+      var balls = [];
+      _(num).times(function(idx) {
+        balls.push({
+          index: idx + 1,
+          selected: false
+        });
+      });
+
+      return balls;
+    }
   }
 })();
---
+++ public/js/utils.js
@@ -0,0 +1,19 @@
+(function() {
+  angular.module('LotteryApp').service('appUtils', appUtils);
+
+  function appUtils() {
+    var that = this;
+    that.factorial = function(num) {
+      var res = 1;
+      _(num).times(function(index) {
+        res = res * (index + 1);
+      })
+
+      return res;
+    };
+
+    that.computeCxy = function(x, y) {
+      return Math.round(that.factorial(x) / that.factorial(y));
+    }
+  }
+})();
